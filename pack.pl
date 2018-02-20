894675faa8e0476420802660a18c7f8e63a40cb4
Wouter Beek <wouter@triply.cc>
2018-02-20 08:23:25 +0100

    DEPLOYMENT: Added Prolog Pack information.

---
+++ pack.pl
@@ -0,0 +1,5 @@
+home('https://github.com/wouterbeek/Pro-GeoSPARQL').
+keywords(['geo-spatial','GeoSPARQL','SPARQL','Semantic Web']).
+name('Pro-GeoSPARQL').
+title('Prolog-based GeoSPARQL implementation').
+version('0.0.1').
